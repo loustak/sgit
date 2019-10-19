083f35e0e56a633d9eff33320a073d2bee407983
Lucas Sardois <sardois.lucas@gmail.com>
2019-10-19 21:46:28 +0200

    The diff algorithm seems to work

--- src/main/scala/com/sardois/sgit/Diff.scala
+++ src/main/scala/com/sardois/sgit/Diff.scala
@@ -1,7 +1,5 @@
 package com.sardois.sgit
 
-import better.files.File
-import com.sardois.sgit.Diff.Diffs
 import com.sardois.sgit.DiffEnum.DiffEnum
 
 import scala.annotation.tailrec
@@ -36,32 +34,51 @@ object Diff {
     def diff(l1: Vector[String], l2: Vector[String]): Diffs = {
 
         @tailrec
-        def rec2(l: String, l2: Vector[String], ln1: Int, ln2: Int, diffs: Diffs): (Vector[String], Diffs) = {
-            if (l2.isEmpty) (l2, diffs :+ Diff(ln1, DiffEnum.ADD, l))
-            else if (l == l2.head) (l2.tail, diffs)
-            else {
-                val newDiff = Diff(ln2, DiffEnum.REMOVE, l2.head)
-                rec2(l, l2.tail, ln1, ln2 + 1, diffs :+ newDiff)
+        def rec(i1: Int, i2: Int, diffs: Diffs): Diffs = {
+            if (i1 == l1.length) {
+                if (i2 == l2.length) return diffs
+                var c = 0
+                return l2.map(str => {
+                    val diff = Diff(i2 + c, DiffEnum.REMOVE, str)
+                    c = c + 1
+                    diff
+                })
             }
-        }
 
-        @tailrec
-        def rec(l1: Vector[String], l2: Vector[String], lineNumber: Int, diffs: Diffs): Diffs = {
-            if (l1.isEmpty) return diffs
+            val h1 = l1(i1)
 
-            val h1 = l1.head
-
-            if (l2.isEmpty) {
-                rec(l1.tail, l2, lineNumber + 1, diffs :+ Diff(lineNumber, DiffEnum.ADD, h1))
-            } else if (h1 == l2.head) {
-                rec(l1.tail, l2.tail, lineNumber + 1, diffs)
+            if (i2 == l2.length) {
+                rec(i1 + 1, i2, diffs :+ Diff(i1, DiffEnum.ADD, h1))
+            } else if (h1 == l2(i2)) {
+                rec(i1 + 1, i2 + 1, diffs)
             } else {
-                val (nl2, newDiff) = rec2(h1, l2, lineNumber, lineNumber, Vector())
-                rec(l1.tail, nl2, lineNumber + 1, diffs :++ newDiff)
+                if (i1 == l1.length - 1) {
+                    // We are on the last element, remove all the remaining elements of l2
+                    var c = 0
+                    val newDiff = l2.map(str => {
+                        val diff = Diff(i2 + c, DiffEnum.REMOVE, str)
+                        c = c + 1
+                        diff
+                    }) :+ Diff(i1, DiffEnum.ADD, h1)
+                    rec(i1 + 1, i2 + c, diffs :++ newDiff)
+                } else {
+                    val idx = l2.indexOf(h1, i2)
+                    if (idx == -1) {
+                        rec(i1 + 1, i2, diffs :+ Diff(i1, DiffEnum.ADD, h1))
+                    } else {
+                        // Found, add all the intermediate elements
+                        var c = 0
+                        val newDiff = l2.slice(i2, idx).map(str => {
+                            val diff = Diff(i2 + c, DiffEnum.REMOVE, str)
+                            c = c + 1
+                            diff
+                        })
+                        rec(i1 + 1, idx + 1, diffs :++ newDiff)
+                    }
+                }
             }
-
         }
 
-        rec(l1, l2, 0, Vector())
+        rec(0, 0, Vector())
     }
 }
